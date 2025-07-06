bcc37c5298c9dfe4d7b390e83454160c5265bc8a
Aquib Jawed <aquibjawed.dev@gmail.com>
2025-07-06 13:41:44 +0530

    feat: mail service added for both mail trap and resend

---
+++ backend/src/utils/sendEmail.js
@@ -0,0 +1,91 @@
+import Mailgen from "mailgen";
+
+import { mailtrapTransporter } from "../config/mailtrapMailer.js";
+import { resendTransporter } from "../config/resendMailer.js";
+import { ApiError } from "./ApiError.js";
+import { env } from "../config/env.js";
+
+export const sendEmail = async ({ email, subject, mailGenContent }) => {
+  const mailGenerator = new Mailgen({
+    theme: "salted",
+    product: {
+      name: "AlgoCraft",
+      link: "https://algocraft.tech",
+      copyright: `Copyright © ${new Date().getFullYear()} AlgoCraft. All rights reserved.`,
+    },
+  });
+
+  const emailHTML = mailGenerator.generate(mailGenContent);
+  const emailText = mailGenerator.generatePlaintext(mailGenContent);
+
+  try {
+    if (env.NODE_ENV.trim().toLocaleLowerCase() === "development") {
+      const info = await mailtrapTransporter.sendMail({
+        from: env.SENDER_EMAIL,
+        to: email,
+        subject,
+        text: emailText,
+        html: emailHTML,
+      });
+
+      if (!info?.messageId) {
+        console.error("Mailtrap failed:", info);
+        throw new ApiError(HTTP_STATUS.BAD_REQUEST, "Failed to send email via Mailtrap", info);
+      }
+    } else {
+      const response = await resendTransporter.emails.send({
+        from: env.SENDER_EMAIL,
+        to: email,
+        subject,
+        text: emailText,
+        html: emailHTML,
+      });
+
+      if (!response || response.error) {
+        console.error("Resend failed:", response?.error);
+        throw new ApiError(HTTP_STATUS.BAD_REQUEST, "Failed to send email via Resend", response);
+      }
+    }
+
+    console.log(`Email sent successfully  to ${email}`);
+  } catch (error) {
+    console.error("Error in sending email:", error);
+    if (error instanceof ApiError) {
+      throw error;
+    }
+    throw new ApiError(HTTP_STATUS.INTERNAL_SERVER_ERROR, "Unable to send email", error);
+  }
+};
+
+export const emailVerificationMailGenContent = (fullname, verificationUrl) => ({
+  body: {
+    name: fullname,
+    intro: "Welcome to AlgoCraft! We're very excited to have you on board.",
+    action: {
+      instructions: "To get started, please verify your email:",
+      button: {
+        color: "#22BC66",
+        text: "Verify your email",
+        link: verificationUrl,
+      },
+    },
+    outro: "Ignore! if you haven't created the account.",
+  },
+});
+
+export const forgotPasswordMailGenContent = (fullname, forgotPasswordUrl) => ({
+  body: {
+    name: fullname,
+    intro: "You recently requested to reset your password for your AlgoCraft account.",
+    action: {
+      instructions: "Click the button below to reset your password:",
+      button: {
+        color: "#22BC66",
+        text: "Reset Password",
+        link: forgotPasswordUrl,
+      },
+    },
+    outro:
+      "If you did not request a password reset, no further action is required. This link will expire shortly for your security.",
+  },
+});
